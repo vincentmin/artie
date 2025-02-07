637200b52754b9777972451e98e40efe7703a98d
Vincent Min <39170736+vincentmin@users.noreply.github.com>
2025-02-07 20:11:47 +0000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # open-data-chatbot
-A chatbot on open data
+
+A chatbot on open data. I have not yet decided which open data source to use yet.
+Options:
+- https://docs.irail.be/ [for example](https://api.irail.be/v1/liveboard/?id=BE.NMBS.008892007&station=Gent-Sint-Pieters&date=070225&time=1230&arrdep=departure&lang=en&format=json&alerts=false)
+- https://www.ns.nl/reisinformatie/ns-api
+- 
---
+++ src/cache.py
@@ -0,0 +1,20 @@
+import functools
+import time
+
+def timed_lru_cache(seconds: int=300):
+    def wrapper(func):
+        @functools.lru_cache()
+        def cached_func(*args, **kwargs):
+            now = time.time()
+            if args in cached_func.cache:  # Check if key exists
+                result, timestamp = cached_func.cache[args]
+                if now - timestamp < seconds:
+                    return result
+                else:
+                    del cached_func.cache[args]  # Remove expired entry
+            # If key not found or expired, calculate and store
+            result = func(*args, **kwargs)
+            cached_func.cache[args] = (result, now)
+            return result
+        return cached_func
+    return wrapper
