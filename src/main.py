11a3dc86f75d2eda6ea7caca24af05cb40c1251a
Vincent Min <39170736+vincentmin@users.noreply.github.com>
2025-02-07 21:36:05 +0000

    first functional chatbot

--- src/main.py
+++ src/main.py
@@ -1,17 +1,31 @@
-import pandas as pd
-import cbsodata
+import chainlit as cl
+from google import genai
+from google.genai.types import Tool, GenerateContentConfig, GoogleSearch
 
-# tables = cbsodata.get_table_list()
-# for table in tables:
-#     if "home" in table["Title"]:
-#         print(f"{table['Identifier']}: {table['Title']}")
+client = genai.Client()
+model_id = "gemini-2.0-flash-001"
 
-data = cbsodata.get_data("85773ENG")
+google_search_tool = Tool(google_search=GoogleSearch())
 
-df = pd.DataFrame(data)
 
-print(df.shape)
-print(df.describe())
-print(df.head())
-print(df.tail())
-print(df.iloc[0])
+@cl.step(type="tool")
+async def tool():
+    # Fake tool
+    await cl.sleep(2)
+    return "Response from the tool!"
+
+
+@cl.on_message  # this function will be called every time a user inputs a message in the UI
+async def main(message: cl.Message):
+    response = await client.aio.models.generate_content(
+        model=model_id,
+        contents=message.content,
+        config=GenerateContentConfig(
+            tools=[google_search_tool],
+            response_modalities=["TEXT"],
+            # system_instruction="You are a helpful assistant",
+        ),
+    )
+
+    await cl.Message(content=response.text).send()
+    # response.candidates[0].grounding_metadata.search_entry_point.rendered_content
