2238879a9a6efaad89e7b838d48d89b644f1168d
Vincent Min <39170736+vincentmin@users.noreply.github.com>
2025-02-15 21:17:39 +0000

    simplify scrape_moma.py

--- scrape_moma.py
+++ scrape_moma.py
@@ -2,28 +2,16 @@
 # requires-python = ">=3.12"
 # dependencies = [
 #     "datasets",
-#     "requests",
+#     "pandas",
 # ]
 # ///
 from datasets import Dataset
-import tempfile
-from pathlib import Path
-import requests
+import pandas as pd
 
 url = (
     "https://github.com/MuseumofModernArt/collection/raw/refs/heads/main/Artworks.json"
 )
-
-with tempfile.TemporaryDirectory() as temp_dir:
-    temp_path = Path(temp_dir) / "Artworks.json"
-
-    response = requests.get(url, stream=True)
-    response.raise_for_status()
-
-    with open(temp_path, "wb") as f:
-        for chunk in response.iter_content(chunk_size=8192):
-            f.write(chunk)
-
-    ds = Dataset.from_json(str(temp_path))
-    print(ds)
-    ds.push_to_hub(repo_id="vincentmin/moma")
+df = pd.read_json(url)
+ds = Dataset.from_pandas(df)
+print(ds)
+ds.push_to_hub(repo_id="vincentmin/moma")
